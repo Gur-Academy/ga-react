5598b670aee5f1503730c3c490cfc675375a8422
mantejjosan <mantejsingh989@gmail.com>
2025-08-11 14:00:20 +0530

    GA_14: Initiate Vite + React + Ts + Vitest Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='flex items-center'>
+      <img className="logo " src="/logo.png" alt="logo" />
+      <h1 className="text-blue-600 text-xl">Gur Academy</h1>
+    </div>
+  )
+}
+
+export default App
