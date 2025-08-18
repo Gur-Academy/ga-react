f0dc0409552592f6c80c035f11252609e7dc7029
Warjot Kaur <curiouscodeseeker@gmail.com>
2025-08-18 16:47:12 +0530

    Update vite.config.js rename folders and files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+import Layout1 from "./layout/layout1";
+import Homepage from './pages/HomePage';
+import Aboutpage from './pages/AboutPage';
+import Contactpage from './pages/ContactPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout1 />} >
+      <Route index element={<Homepage />} />
+      <Route path='/About' element={<Aboutpage />} />
+      <Route path='/Contact' element={<Contactpage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
