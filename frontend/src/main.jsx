9a140a176aaa441e3bd920668b371ea2664e9857
Warjot Kaur <curiouscodeseeker@gmail.com>
2025-08-18 16:46:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,30 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom';
-
-import Layout1 from "./Layout/layout1";
-import Homepage from './Pages/Homepage';
-import Aboutpage from './Pages/Aboutpage';
-import Contactpage from './Pages/Contactpage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout1 />} >
-      <Route index element={<Homepage />} />
-      <Route path='/About' element={<Aboutpage />} />
-      <Route path='/Contact' element={<Contactpage />} />
-    </Route>
-  )
-);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
